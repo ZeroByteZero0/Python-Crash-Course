28e29dc7411b705b2fab5cec9076a0e1ddfa587a
ZeroByteZero0 <mj_j@live.com>
2025-02-18 14:14:29 -0800

    updated numbers_crashcourse

--- CH 2/3numbers_crashcourse.py
+++ CH 2/3numbers_crashcourse.py
@@ -36,4 +36,44 @@ print(51//2)
 
 #INTEGERS AND UNDERSCORES
 #   *Python treats integer values with underscores the same as those without
-#   *20_000 is the same as 20000 to Python
+#   *20_000 is the same as 20000 to Python, this is the same for floats as well
+
+#MULTIPLE ASSIGNMENTS
+#   *Python allows for multiple variable assignments to values in a single line of code
+#   *separate variable names and values with commas
+x,y,z = 0,0,0
+print(x,y,z)
+
+#CONSTANTS
+#   *constants are variables whose values stay the same throughout the lifetime of a program
+#   *Python doesn't keep track of constants
+#   *a good way to differentiate from constants and regular variables is by naming with all capitals
+MAX_VALUE = 1000
+
+#COMMENTS
+#   *(#) indicates comments in Python, anything following a (#) will be ignored by Py interpreter
+#   *It's a good idea to comment on larger programs and lines of code to explain functionality
+#   *"Writing clear, concise comments in your code is one of the most beneficial habits you can form as a new programmer"
+
+#THE ZEN OF PYTHON
+#   The Zen of Python, by Tim Peters
+#   Beautiful is better than ugly.
+#   Explicit is better than implicit.
+#   Simple is better than complex.
+#   Complex is better than complicated.
+#   Flat is better than nested.
+#   Sparse is better than dense.
+#   Readability counts.
+#   Special cases aren't special enough to break the rules.
+#   Although practicality beats purity.
+#   Errors should never pass silently.
+#   Unless explicitly silenced.
+#   In the face of ambiguity, refuse the temptation to guess.
+#   There should be one-- and preferably only one --obvious way to do it.
+#   Although that way may not be obvious at first unless you're Dutch.
+#   Now is better than never.
+#   Although never is often better than *right* now.
+#   If the implementation is hard to explain, it's a bad idea.
+#   If the implementation is easy to explain, it may be a good idea.
+#   Namespaces are one honking great idea -- let's do more of those!
+
